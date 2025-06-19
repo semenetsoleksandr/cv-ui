478aa7ee2bde4d1fa991c4561466e809133ad7c3
Olek Semenets <bobrt440@gmail.com>
2025-06-19 16:04:25 +0200

    edit Contact Form, add photo_cv

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,15 +1,22 @@
-
-
 export const ContactForm = () => {
     return (
-        <form><h1>ContactForm</h1>
-            <label>Your Name</label>
-            <input type="text" placeholder="Enter your name"/>
-            <label>Your Email</label>
-            <input type="email" placeholder="Enter your email"/>
-            <label>Your message</label>
-            <textarea placeholder="Enter your message"></textarea>
-            <button type="submit">Send message</button>
-        </form>
+        <section>
+            <form className="contact">
+                <h1>ContactForm</h1>
+                <div className="input-box">
+                <label>Your Name</label>
+                <input type="text" className="field" placeholder="Enter your name"/>
+                </div>
+                <div className="input-box">
+                <label>Your Email</label>
+                <input type="email" className="field" placeholder="Enter your email"/>
+                </div>
+                <div className="input-box">
+                <label>Your message</label>
+                <textarea className="field mes" placeholder="Enter your message"></textarea>
+                </div>
+                <button type="submit">Send message</button>
+            </form>
+        </section>
     )
 }
--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -10,6 +10,7 @@ interface OverviewProps {
 const Overview: React.FC<OverviewProps> = ({ title, introText }) => {
     return (
         <section>
+            <img className="photo cv" src="/photo_cv.png" alt="photo cv"/>
             <p><em>{title}</em></p>
             <p>{introText}</p>
         </section>
